2f6790060c0ed74dc2a63a56375740b1c21e770c
Pepee1920 <017100363k@uandina.edu.pe>
2022-10-10 16:42:30 -0500

    Commit 10/10

--- src/app/components/admin/paquetes/paquetes.component.ts
+++ src/app/components/admin/paquetes/paquetes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Packet as Packet} from 'src/app/model/admin.model';
+
 @Component({
   selector: 'app-paquetes',
   templateUrl: './paquetes.component.html',
@@ -8,6 +10,62 @@ import { Component, OnInit } from '@angular/core';
 export class PaquetesComponent implements OnInit {
 
   constructor() { }
+
+  packet: Packet[] = [
+    {
+      idPacket: 1,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    },
+    
+    {
+      idPacket: 2,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    },
+
+    {
+      idPacket: 3,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    },
+
+    {
+      idPacket: 4,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    },
+
+    {
+      idPacket: 5,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    },
+
+    {
+      idPacket: 6,
+      nombreDestino: "Machupicchu",
+      tiempo: "Full Day",
+      precioDestino: 550,
+      hora: 4,
+      min: 0,
+    }
+  ];
 
   ngOnInit(): void {
   }
--- src/app/model/admin.model.ts
+++ src/app/model/admin.model.ts
@@ -1,4 +1,5 @@
 export interface Packet {
+    idPacket: number;
     nombreDestino: string;
     tiempo: string;
     precioDestino: number;
