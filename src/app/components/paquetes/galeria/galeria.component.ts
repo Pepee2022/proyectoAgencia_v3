fbeeddb3c6666a9ce538d51481a08bed58ef1474
Pepee1920 <017100363k@uandina.edu.pe>
2022-11-04 19:32:41 -0500

    Commit 04/10

--- src/app/components/paquetes/galeria/galeria.component.ts
+++ src/app/components/paquetes/galeria/galeria.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter ,Input ,OnInit, Output } from '@angular/core';
 import { Packet } from 'src/app/model/admin.model';
-import { StoreService } from 'src/app/services/store.service';
 import { PacketDataService } from 'src/app/services/packet-data.service';
 
 @Component({
